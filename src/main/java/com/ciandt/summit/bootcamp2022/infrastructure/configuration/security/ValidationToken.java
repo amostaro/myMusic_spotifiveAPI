2a182337fc84f78bb96f964fc132c2de608dfd00
amostaro <amostaro@ciandt.com>
2022-09-15 11:33:13 -0300

    update 15 set v5

--- src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/ValidationToken.java
+++ src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/configuration/security/ValidationToken.java
@@ -2,12 +2,14 @@ package com.ciandt.summit.bootcamp2022.infrastructure.configuration.security;
 
 import com.ciandt.summit.bootcamp2022.infrastructure.configuration.feign.AuthenticationApiIntegration;
 import com.ciandt.summit.bootcamp2022.infrastructure.configuration.security.dto.CreateAuthorizerRequest;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,7 +21,6 @@ public class ValidationToken {
     private AuthenticationApiIntegration authenticationApiIntegration;
 
     void validationTokenMethod(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, CreateAuthorizerRequest data) throws IOException {
-
         try {
             authenticationApiIntegration.isValidToken(data);
             UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
@@ -28,8 +29,11 @@ public class ValidationToken {
             SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (FeignException.Unauthorized e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "The token is not valid.");
+        } catch (ServletException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
