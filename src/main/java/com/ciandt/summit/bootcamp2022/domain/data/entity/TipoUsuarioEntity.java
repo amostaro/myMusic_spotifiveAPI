b2abb8d265c3799bd5ee925b6f6a0174c88d269a
amostaro <amostaro@ciandt.com>
2022-09-15 13:45:14 -0300

    update 15 set v9

--- src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/TipoUsuarioEntity.java
+++ src/main/java/com/ciandt/summit/bootcamp2022/domain/data/entity/TipoUsuarioEntity.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Set;
 
 @Getter
 @Setter
 @Entity(name = "TipoUsuario")
 @Table(name = "TipoUsuario")
-public class TipoUsuarioEntity {
+public class TipoUsuarioEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
