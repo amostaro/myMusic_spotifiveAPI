9d49300872a80695bd9ce47651217cc0f29ea071
amostaro <amostaro@ciandt.com>
2022-09-16 07:37:05 -0300

    .gitignore funcionando

--- src/main/java/com/ciandt/summit/bootcamp2022/application/controller/UserController.java
+++ src/main/java/com/ciandt/summit/bootcamp2022/application/controller/UserController.java
@@ -3,10 +3,21 @@ package com.ciandt.summit.bootcamp2022.application.controller;
 import com.ciandt.summit.bootcamp2022.domain.port.interfaces.UserServicePort;
 import com.ciandt.summit.bootcamp2022.domain.service.exception.UserNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+@ApiResponses(
+        value = {
+                @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso"),
+                @ApiResponse(responseCode = "401", description = "Você não tem permissão para acessar este recurso"),
+                @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção"),
+                @ApiResponse(responseCode = "204", description = "Não foi encontrado nenhum dado")
+        }
+)
 
 @RequiredArgsConstructor
 @RestController
@@ -15,7 +26,7 @@ public class UserController {
 
     private final UserServicePort userServicePort;
 
-    @Operation(description = "")
+    @Operation(description = "Altera o tipo de usuario")
     @PutMapping("/{userId}")
     public ResponseEntity<String> updateUserType(@PathVariable String userId, @RequestParam String userTypeId ) throws UserNotFoundException {
 
@@ -24,10 +35,12 @@ public class UserController {
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
 
-    @Operation(description = "")
+    @Operation(description = "Retorna o Id do tipo de usuario")
     @GetMapping("/{userId}")
     public ResponseEntity<String> getUserType(@RequestParam @PathVariable String userId) {
+
         var getUser = userServicePort.verifyUserType(userId);
+
         return new ResponseEntity<>(getUser, HttpStatus.OK);
     }
 }
